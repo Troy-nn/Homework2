573dc856d92c46b27bee2a48a8aea8f4d9b514dd
Troy-nn <nrgnnrf@gmail.com>
2024-07-08 00:11:16 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,9 +1,5 @@
-summa_DZ = "12"
-print(summa_DZ)
-time_work = "1.5"
-print(time_work)
+summa_DZ = 12
+time_work = 1.5
 kurs_name = "Python"
-print(kurs_name)
-print(1.5 / 12)
-time_for_task = 0.125
+time_for_task = (time_work / summa_DZ)
 print('Курс:',kurs_name,'всего задач:',summa_DZ,', затрачено часов:',time_work, ', среднее время выполнения',time_for_task,'часа.')
